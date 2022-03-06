43d485ac6b829cc9b571e8dab5e593be73839e91
ezexedge <ezeedge@gmail.com>
2022-03-05 23:08:17 -0300

    eliminando carpetas

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
-import { ProductoModule } from './producto/producto/producto.module';
+import { ProductoModule } from './producto/producto.module';
 
 @Module({
   imports: [UserModule,
